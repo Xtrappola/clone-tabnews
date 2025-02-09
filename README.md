0ef1e3059b26c8b94a59398169b62f07a51446d8
Guilherme Vieira <22782014+Xtrappola@users.noreply.github.com>
2025-02-09 23:04:20 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Tabnews implementation 
+
+Tabnews implementation
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Is something bad about to happen to me</h1>
+  return <h1>Is something bad about to happen to me</h1>;
+}
+
+function teste() {
+  console.log("Teste");
+}
+
+function teste2() {
+  console.log("Intendação errada");
 }
 
 export default Home;
